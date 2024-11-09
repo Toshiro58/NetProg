5d2d47f0b07cc324d1bc76a87285bda0c4beb01e
Toshiro58 <dorog2283222@gmail.com>
2024-11-09 17:33:56 +0300

    Update z.cpp

--- daytime/z.cpp
+++ daytime/z.cpp
@@ -2,17 +2,18 @@
 #include <cstring>
 #include <arpa/inet.h>
 #include <unistd.h>
-using namespace std;
 
 #define PORT 13
-#define SERVER_IP "127.0.0.1"
+#define SERVER_IP "127.16.40.1"
+#define BUFFER_SIZE 256
 
 int main() {
     int s = socket(AF_INET, SOCK_DGRAM, 0);
     if (s == -1) {
-        cerr << "Ошибка создания сокета" << endl;
+        std::cerr << "Ошибка создания сокета" << std::endl;
         return 1;
     }
+
     struct sockaddr_in self_addr;
     memset(&self_addr, 0, sizeof(self_addr));
     self_addr.sin_family = AF_INET;
@@ -20,25 +21,47 @@ int main() {
     self_addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(s, (const struct sockaddr*)&self_addr, sizeof(self_addr)) == -1) {
-        cerr << "Ошибка привязки сокета" << endl;
+        std::cerr << "Ошибка привязки сокета" << std::endl;
         close(s);
         return 1;
     }
+
     struct sockaddr_in srv_addr;
     memset(&srv_addr, 0, sizeof(srv_addr));
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_port = htons(PORT);
     srv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
     if (connect(s, (const struct sockaddr*)&srv_addr, sizeof(srv_addr)) == -1) {
-        cerr << "Ошибка установки соединения" << endl;
+        std::cerr << "Ошибка установки соединения" << std::endl;
+        close(s);
+        return 1;
+     }
+
+    std::string message;
+    std::cout << "Введите сообщение: ";
+    std::getline(std::cin, message);
+
+    char msg[message.size() + 1];
+    strcpy(msg, message.c_str());
+
+    if (sendto(s, msg, strlen(msg), 0, (const struct sockaddr*)&srv_addr, sizeof(srv_addr)) == -1) {
+        std::cerr << "Ошибка отправки данных" << std::endl;
         close(s);
         return 1;
     }
-    char msg[] = "hello\n";
-    send(s, msg, sizeof(msg), 0);
-    char buf[256];
-    recv(s, buf, sizeof(buf), 0);
-    cout << "Ответ от сервера: " << buf << endl;
+
+    char buf[BUFFER_SIZE];
+    socklen_t len = sizeof(srv_addr);
+    if (recvfrom(s, buf, BUFFER_SIZE, 0, (struct sockaddr*)&srv_addr, &len) == -1) {
+        std::cerr << "Ошибка получения данных" << std::endl;
+        close(s);
+        return 1;
+    }
+
+    buf[BUFFER_SIZE - 1] = '\0'; 
+    std::cout << "Ответ от сервера: " << buf << std::endl;
+
     close(s);
     return 0;
 }
